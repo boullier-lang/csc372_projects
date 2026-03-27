76c1b894dc2e7319da1a69b4822ca6057190bd86
boullier-lang <mathew_boullier@uri.edu>
2026-03-26 21:16:11 -0400

    made a typo on booking and services for a required file

--- client_site/booking.php
+++ client_site/booking.php
@@ -8,7 +8,7 @@
 	$pageStyles = ['booking.css'];
     require_once 'partials/header.php';
     require_once 'partials/navbar.php';
-	require_once __DIR__ . '/require/ServiceCategory.php';
+	require_once __DIR__ . '/require/serviceCategory.php';
 	
 	$json = file_get_contents('data/services.json');
     $data = json_decode($json, true);
